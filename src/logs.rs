ff4ca237c61dec7861fc20a6154b40fd31b59123
Guillaume (Goliver) Golay <goliworks@gmail.com>
2025-06-04 04:04:02 +0200

    feat: tracing optimization

--- src/logs.rs
+++ src/logs.rs
@@ -3,17 +3,25 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Layer};
 
 pub fn start_logs(path: String) -> WorkerGuard {
     let appender = rolling::never(path, "logs.log");
-    let (non_blocking, guard) = tracing_appender::non_blocking(appender);
+    let (non_blocking, guard) = tracing_appender::non_blocking::NonBlockingBuilder::default()
+        .buffered_lines_limit(2048)
+        .lossy(true)
+        .finish(appender);
 
     let terminal_filter = EnvFilter::new("quark=trace");
+
     let file_filter = EnvFilter::new("quark=info");
 
     let terminal_layer = tracing_subscriber::fmt::layer()
+        .with_file(false)
         .with_writer(std::io::stdout)
         .with_filter(terminal_filter);
+
     let file_layer = tracing_subscriber::fmt::layer()
         .with_writer(non_blocking)
         .with_ansi(false)
+        .with_file(false)
+        .with_line_number(false)
         .with_filter(file_filter);
 
     let subscriber = tracing_subscriber::registry()
