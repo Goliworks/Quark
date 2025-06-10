caf6ad9e5b77d30feddf24c433a757a55467378c
Guillaume (Goliver) Golay <goliworks@gmail.com>
2025-06-10 20:18:06 +0200

    chore: build script

---
+++ tools/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TMP_PACKAGE_DIR="tmp_package"
+RELEASE_PATH="target/release/quark"
+
+echo "Building Quark"
+cargo build --release
+
+if [ $? -eq 0 ]; then
+  echo "Quark built successfully"
+else
+  echo "Quark build failed"
+  exit 1
+fi
+
+if [ -f "$RELEASE_PATH" ]; then
+  mkdir -p "$TMP_PACKAGE_DIR"
+  cp "$RELEASE_PATH" "$TMP_PACKAGE_DIR/quark"
+else
+  echo "Quark binary not found in $PWD"
+  exit 1
+fi
+
+echo "Packaging Quark"
+
+# Get version from Cargo.toml
+VERSION=$(awk '
+  /^\[package\]/ { in_package = 1; next }
+  /^\[/ { in_package = 0 }
+  in_package && /^version[[:space:]]*=/ {
+    match($0, /"[^\"]+"/)
+    print substr($0, RSTART+1, RLENGTH-2)
+    exit
+  }
+' Cargo.toml)
+
+cp -r package/* "$TMP_PACKAGE_DIR/"
+
+mkdir -p dist
+
+tar -czvf "dist/quark-$VERSION.tar.gz" -C "$TMP_PACKAGE_DIR" .
+rm -rf "$TMP_PACKAGE_DIR"
+
+echo "Quark packaged successfully"
